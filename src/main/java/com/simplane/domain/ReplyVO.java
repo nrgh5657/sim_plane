e8574e71a1c682eed4e833aa4af7060b153614cd
Lee <tlsrl930@gmail.com>
2025-06-30 20:46:03 +0900

    댓글자바코드, 회원가입 구현

--- src/main/java/com/simplane/domain/ReplyVO.java
+++ src/main/java/com/simplane/domain/ReplyVO.java
@@ -1,7 +1,15 @@
 package com.simplane.domain;
 
+import lombok.*;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReplyVO {
 
     private Long replyid;
